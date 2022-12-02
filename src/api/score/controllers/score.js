38b5fea86bddb75d0656f3acdd6140dabc4af837
Edwin Mugambi <edwinnjeru77@gmail.com>
2022-12-02 14:03:03 +0300

    extended the score find() and findOne() controller :artist:

--- src/api/score/controllers/score.js
+++ src/api/score/controllers/score.js
@@ -6,7 +6,44 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-//extending the create score controllers
+//helper function for scoring
+/**
+ * Calculates the scores for the find and findOne
+ * Score controllers
+ */
+function calculateScore(score){
+    let question;
+    let scoreTotal = 0;
+
+    score.answer.map((userAnswer) => {
+        question = userAnswer.question;
+
+        if(question.answer === userAnswer.value) {
+            userAnswer.correct = true;
+            scoreTotal += 1;
+        }else {
+            userAnswer.correct = false;
+        }
+
+        userAnswer.correctValue = question.answer;
+        delete userAnswer.question
+        return userAnswer
+    })
+
+    const questionCount = score.quiz.questions.length;
+    delete score.quiz.questions;
+
+    let resp = {
+        questionCount,
+        scoreTotal,
+        quiz: score.quiz
+    }
+
+    return { ...resp, score}
+}
+
+
+//extending the score controllers
 module.exports = createCoreController('api::score.score', ({ strapi }) => {
 
     //extending the create controller
@@ -65,7 +102,48 @@ module.exports = createCoreController('api::score.score', ({ strapi }) => {
         }
     }
 
+    //extending the findOne controller
+    async findOne(ctx) {
+        const {id} = ctx.params;
 
+        try{
+            let score = await strapi.service('api::score.score').findOne(id, {
+                filters : {user: ctx.state.user.id },
+                populate : {
+                    answers : {populate : ['question']},
+                    quiz : {populate: ['questions']}
+                }
+            });
+
+            return calculateScore(score)
+        } catch{
+            ctx.response.status = 500;
+            return { error : { message : 'There was a problem fetching your score.'}}
+        }
+    }
+
+    //extend the find score controller
+    async find(ctx) {
+        try {
+            const {query} = ctx;
+
+            let scores = await strapi.service('api::score.score').find({
+                filters : {user : ctx.state.user.id},
+                populate : {
+                    answers: {populate :['question']},
+                    quiz: {populate : ['questions']}
+                },
+                ...query
+            });
+
+            return scores.results.map(calculateScore);
+        }catch{
+            ctx.response.status = 500;
+            return { error : {message : 'There was a problem fetching your scores. '}}
+        }
+    }
+
+    
 
 
 } );
