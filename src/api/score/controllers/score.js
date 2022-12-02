3111f5938cc3a4f694848a07f621a6f0305e0f81
Edwin Mugambi <edwinnjeru77@gmail.com>
2022-12-02 11:51:38 +0300

    strapi cmc backend :artist:

---
+++ src/api/score/controllers/score.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * score controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::score.score' , ({strapi}) => {
+    //Extending the Score Controller
+});
