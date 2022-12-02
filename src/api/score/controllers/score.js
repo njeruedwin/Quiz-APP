f8f01f412af37657f1d521c13d26a3d6f989fd15
Edwin Mugambi <edwinnjeru77@gmail.com>
2022-12-02 12:47:54 +0300

    extended the score create controller :artist:

--- src/api/score/controllers/score.js
+++ src/api/score/controllers/score.js
@@ -6,6 +6,68 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::score.score' );
+//extending the create score controllers
+module.exports = createCoreController('api::score.score', ({ strapi }) => {
+
+    //extending the create controller
+    async create(ctx) {
+        let {answers} = ctx.request.body;
+        const quizId = ctx.request.body.quiz.id;
+
+        try {
+            let quiz  = await strapi.service('api::quiz.quiz').findOne(quizId, {populate : ['questions']});
+
+            let score = await strapi.service('api::score.score').create({
+                data : {
+                    quiz : quizId,
+                    user: ctx.state.user.id
+                }
+            });
+
+            let question, userAnswer;
+            let scoreTotal = 0;
+
+            score.answers = [];
+
+            for (let i = 0 ; i < answers.length ; i++ ){
+                userAnswer = answers[i];
+
+                question = quiz.questions.find((qst)=> qst.id === userAnswer.question.id );
+                if(question){
+                    await strapi.service('api::answer.answer').create({
+                        data : {
+                            question : question.id,
+                            score : score.id,
+                            value : userAnswer.value
+                        }
+                    });
+
+                    if (question.answer === userAnswer.value){
+                        userAnswer.correct = true;
+                        scoreTotal += 1;
+                    }else {
+                        userAnswer.correct = false;
+                    }
+                    userAnswer.correctValue = question.answer;
+                }
+
+                score.answers.push(userAnswer)
+
+            }
+
+            const questionsCount = quiz.questions.length;
+            delete quiz.questions;
+
+            return { questionCount, scoreTotal, quiz, score}
+        } catch {
+            ctx.response.status = 500;
+            return {error : {message : 'There was a problem scoring your answer'}}
+        }
+    }
+
+
+
+
+} );
 
 //commit 
